853208f145271de2b601fd6a0637628cd962786c
alu0100406122 <alu0100406122@ull.edu.es>
2016-05-13 18:40:41 +0000

    Estructura de usuarios. Formato de Login en el index.ejs

--- app.js
+++ app.js
@@ -36,6 +36,63 @@ app.get('/', (request, response) => {
 });
 
 
+// Usuarios -----------------------------------------------------------------------
+
+app.param('usuario',function(req,res,next,usuario)
+{
+    if (usuario.match(/^[a-z0-9_]*$/i)) { 
+      req.ejemplo = usuario;
+    } else { 
+      next(new Error(`<${usuario}> does not match 'usuario' requirements`));
+    }
+    next();
+});
+
+app.get('/buscar/:usuario',(request,response) => {
+    console.log("Usuario a buscar:"+request.params.usuario);
+    User.find({nombre: request.params.usuario},function(err,data) {
+        if(err) {
+            console.error("Se ha producido un error->"+err);
+        }
+        else {
+            if(data.length > 0){
+                console.log("Error:"+err);
+                console.log("Enviando datos a csv.js => Id de usuario:"+data[0]._id);
+                /*var id = new ObjectId(data[0]._id);
+                console.log("Id:"+id._id);*/
+                const id = mongoose.Types.ObjectId(data[0]._id);
+                console.log("Id:"+id);
+                Tabla.find({_creator: id},function(err,data_tablas)
+                {
+                    if(err)
+                    {
+                        console.error("Se ha producido un error->"+err);
+                    }
+                    else
+                    {
+                        console.log("Enviando dattos a csv.js => Tablas asociadas:"+data_tablas);    
+                    }
+                    response.send({contenido: data_tablas, usuario_propietario: id, mensaje_respuesta: "Busqueda realizada correctamente.", mensaje_guardado: " "});
+                });
+            }
+            else{
+                console.log("creando nuevo usuario.");
+                let nuevo_usuario = new User({
+                    nombre: request.params.usuario
+                });
+                nuevo_usuario.save(function(err){
+                   if(err) return console.log(err);
+                   response.send({contenido: " ", usuario_propietario: nuevo_usuario._id, mensaje_respuesta: "Nuevo usuario creado.", mensaje_guardado: " "});
+                }).then(() => {
+                    console.log(`Saved: ${nuevo_usuario}`);
+                })
+            }
+        }
+    });
+});
+
+// ---------------------------------------------------------------------------------------
+
 app.listen(app.get('port'), () => {
     console.log(`Node app is running at localhost: ${app.get('port')}` );
 });
